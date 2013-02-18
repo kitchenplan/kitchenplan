e455db416e9f709058c7b3337f5e2cff92870250
Roderik van der Veer <roderik@vanderveer.be>
2013-02-18 20:34:38 +0100

    update cookbooks

--- vendor/cookbooks/applications/recipes/sequelpro.rb
+++ vendor/cookbooks/applications/recipes/sequelpro.rb
@@ -1,5 +1,5 @@
 dmg_package "Sequel Pro" do
-  volumes_dir "Sequel Pro 1.0"
+  volumes_dir "Sequel Pro 1.0.1"
   owner node['current_user']
   source "http://sequel-pro.googlecode.com/files/sequel-pro-1.0.1.dmg"
   action :install
