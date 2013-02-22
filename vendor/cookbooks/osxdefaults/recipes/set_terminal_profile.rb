539f576e14232acc601ba6d521f8137642f37eed
Roderik van der Veer <roderik@Roderiks-MacBook-Air.local>
2013-02-22 22:05:40 +0100

    Update cookbooks

---
+++ vendor/cookbooks/osxdefaults/recipes/set_terminal_profile.rb
@@ -0,0 +1,11 @@
+osxdefaults_defaults "Default Window Settings" do
+  domain 'com.apple.terminal'
+  key 'Default Window Settings'
+  string 'Pro'
+end
+
+osxdefaults_defaults "Startup Window Settings" do
+  domain 'com.apple.terminal'
+  key 'Startup Window Settings'
+  string 'Pro'
+end
