3cff0173c73fca8b1d2c70688dfd2860f3f5bdc4
Steven Wagner <steven.wagner@disney.com>
2014-01-06 23:50:16 +0100

    Platform-specific code now prettyprints LoadErrors.

--- lib/kitchenplan.rb
+++ lib/kitchenplan.rb
@@ -2,4 +2,14 @@ require 'kitchenplan/mixins'
 require 'kitchenplan/config'
 # platform-specificity
 require 'kitchenplan/platform'
+begin
 require "kitchenplan/platform/#{Kitchenplan::Config.new().platform}"
+rescue LoadError
+	raise "Unsupported platform or fatal error loading support for platform '#{Kitchenplan::Config.new().platform}' ..."
+end
+class Kitchenplan
+	attr_accessor :platform
+	def initialize
+		self.platform = eval("Kitchenplan::Platform::#{Kitchenplan::Platform.constants.first.to_s}.new()")
+	end
+end
