e7fc1c84a580bd380fc442e2c0b069e234ae488c
Steven Wagner <steven.wagner@disney.com>
2014-04-09 02:20:36 +0200

    Now testing detect_platform.

--- spec/unit/kitchenplan_spec.rb
+++ spec/unit/kitchenplan_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
+require 'ohai'
 require 'kitchenplan'
 
 describe Kitchenplan do
-	describe 'sudo' do
-		# foo
+	describe "detect_platform" do
+		let(:system_ohai) { Ohai::System.new() }
+		it "uses system ohai if the passed ohai param is nil" do
+			system_ohai.require_plugin("os")
+			system_ohai.require_plugin("platform")
+			kp = Kitchenplan.new(ohai=nil)
+			kp.detect_platform(ohai=nil)
+			expect(kp.platform.ohai).to match system_ohai
+		end
 	end
+		
 end
