7afff93a7f3ea7aadbb42f3c914dd3b3475f73b8
Steven Wagner <steven.wagner@disney.com>
2014-04-10 09:42:33 +0200

    Add a small test Kitchenplan config repo to exercise the loader.
    I know fixtures aren't as cool as factories but this seems simple enough.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,7 @@ require 'chefspec'
 SimpleCov.start
 require 'kitchenplan'
 
+FIXTURE_CONFIG_DIR = File.join((File.expand_path("../", Pathname.new(__FILE__).realpath)), "/support/fixtures/config")
+
+Dir["./spec/support/**/*.rb"].each {|f| require f}
 
