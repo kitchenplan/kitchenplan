74f42198db3e07e1e643024915b8801e23ce1db8
Steven Wagner <steven.wagner@disney.com>
2014-04-10 09:49:34 +0200

    Clean up setting of defaults, fix config_dir parameter.

--- lib/kitchenplan/mixin/optparse.rb
+++ lib/kitchenplan/mixin/optparse.rb
@@ -8,13 +8,18 @@ class Kitchenplan
     module Optparse
       # parse the command line with OptionParser and return the resulting config.
       def parse_commandline
-	options = {}
+	options = { 
+	:debug => false,
+	:config_dir => "config",
+	:chef => true
+	}
 	OptionParser.new do |opts|
 	  opts.banner = 'Usage: kitchenplan [options]'
 	  opts.on("-d", "--debug", "Show debug information") do |debug|
 	    options[:debug] = debug
 	  end
-	  opts.on("-c", "--config-dir", "Path to YAML config directory") do |config_dir|
+	  options[:config_dir] = "config/"
+	  opts.on("-c [DIRECTORY]", "--config-dir", "Path to YAML config directory") do |config_dir|
 	    options[:config_dir] = config_dir
 	  end
 	  opts.on("-u", "--update-cookbooks", "Update the Chef cookbooks") do |update_cookbooks|
@@ -23,7 +28,6 @@ class Kitchenplan
 	  opts.on("--recipes x,y,z", Array, "Run Kitchenplan with all attributes, but only the recipes passed along on the command line. Useful for testing or fast runs.") do |list|
 	            options[:recipes] = list
 	  end
-	  options[:chef] = true
 	  opts.on("--[no-]chef", "Run chef (defaults to yes)") do |chef|
 	    options[:chef] = chef
 	  end
@@ -34,7 +38,7 @@ class Kitchenplan
 	    exit
 	  end
 	  opts.on_tail("--version", "Show version") do
-	    puts "2.0.1"
+	    puts Kitchenplan::Version::VERSION
 	    exit
 	  end
 	end.parse!
